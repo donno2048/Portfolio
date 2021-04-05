51219023f3e506d500803a2c190faf18c76e9b5b
Elisha Hollander <just4now666666@gmail.com>
2021-04-05 16:31:21 +0300

    Revert "Remove Unnecessary import"

--- src/js/tilt.js
+++ src/js/tilt.js
@@ -1,2 +1,10 @@
-const initTilt = () => {$('.project-wrapper__image a div').tilt({maxTilt: 3,});};
+import tilt from 'tilt.js';
+
+const initTilt = () => {
+  // Projects images
+  $('.project-wrapper__image a div').tilt({
+    maxTilt: 3,
+  });
+};
+
 export default initTilt;
