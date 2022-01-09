9e0db4c6c0423129c49b5c78c379f819913a1296
donno2048 <just4now666666@gmail.com>
2022-01-09 13:55:46 +0200

    put `tilt` in `try`-`catch` and `call` it

--- src/js/tilt.js
+++ src/js/tilt.js
@@ -1,4 +1,4 @@
 import tilt from 'tilt.js';
-tilt; // make code scanning not to say import never used
+try {tilt();} catch {} // make code scanning not to say import never used
 const initTilt = () => {$('img,h1,.about-wrapper__info-text').tilt();};
 export default initTilt;
