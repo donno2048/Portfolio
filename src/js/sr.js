66b413cfe3bc4a6caf1bd0cee64ce9ec71472d1d
Elisha Hollander <just4now666666@gmail.com>
2022-04-13 20:21:48 +0300

    reduce delays by a half
    I should make that automatic!!!

--- src/js/sr.js
+++ src/js/sr.js
@@ -15,57 +15,57 @@ export default function() {
   ScrollReveal().reveal('#about>.container>.section-title',
     assignProps(
       {
-        delay: 2000
+        delay: 1250
       }, defaultProps)
   );
   ScrollReveal().reveal('.about-wrapper__image',
     assignProps(
       {
-        delay: 2500
+        delay: 1500
       }, defaultProps)
   );
   ScrollReveal().reveal('.about-wrapper__info',
     assignProps(
       {
-        delay: 2500,
+        delay: 1500,
         origin: 'left'
       }, defaultProps)
   );
   ScrollReveal().reveal('.project-wrapper>.section-title',
     assignProps(
       {
-        delay: 3500
+        delay: 2000
       }, defaultProps)
   );
   ScrollReveal().reveal('.project-wrapper__text',
     assignProps(
       {
-        delay: 4000
+        delay: 2250
       }, defaultProps)
   );
   ScrollReveal().reveal('#games>.container>.section-title',
     assignProps(
       {
-        delay: 7000
+        delay: 3750
       }, defaultProps)
   );
   ScrollReveal().reveal('#games>.container>.col',
     assignProps(
       {
-        delay: 7500,
+        delay: 4000,
         origin: 'left'
       }, defaultProps)
   );
   ScrollReveal().reveal('#contact>.container>.section-title',
     assignProps(
       {
-        delay: 8500
+        delay: 4500
       }, defaultProps)
   );
   ScrollReveal().reveal('.contact-wrapper',
     assignProps(
       {
-        delay: 9000
+        delay: 4750
       }, defaultProps)
   );
 }
