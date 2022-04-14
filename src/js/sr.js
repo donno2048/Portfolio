5fb842fcd56e81429f442c25d218e192fa6befc5
Elisha Hollander <just4now666666@gmail.com>
2022-04-14 21:52:23 +0300

    remove `delay`

--- src/js/sr.js
+++ src/js/sr.js
@@ -12,60 +12,23 @@ export default function() {
         delay: 500
       }, defaultProps)
   );
-  ScrollReveal().reveal('#about>.container>.section-title',
-    assignProps(
-      {
-        delay: 1250
-      }, defaultProps)
-  );
-  ScrollReveal().reveal('.about-wrapper__image',
-    assignProps(
-      {
-        delay: 1500
-      }, defaultProps)
-  );
+  ScrollReveal().reveal('#about>.container>.section-title', defaultProps);
+  ScrollReveal().reveal('.about-wrapper__image', defaultProps);
   ScrollReveal().reveal('.about-wrapper__info',
     assignProps(
       {
-        delay: 1500,
         origin: 'left'
       }, defaultProps)
   );
-  ScrollReveal().reveal('.project-wrapper>.section-title',
-    assignProps(
-      {
-        delay: 2000
-      }, defaultProps)
-  );
-  ScrollReveal().reveal('.project-wrapper__text',
-    assignProps(
-      {
-        delay: 2250
-      }, defaultProps)
-  );
-  ScrollReveal().reveal('#games>.container>.section-title',
-    assignProps(
-      {
-        delay: 3750
-      }, defaultProps)
-  );
+  ScrollReveal().reveal('.project-wrapper>.section-title', defaultProps);
+  ScrollReveal().reveal('.project-wrapper__text', defaultProps);
+  ScrollReveal().reveal('#games>.container>.section-title', defaultProps);
   ScrollReveal().reveal('#games>.container>.col',
     assignProps(
       {
-        delay: 4000,
         origin: 'left'
       }, defaultProps)
   );
-  ScrollReveal().reveal('#contact>.container>.section-title',
-    assignProps(
-      {
-        delay: 4500
-      }, defaultProps)
-  );
-  ScrollReveal().reveal('.contact-wrapper',
-    assignProps(
-      {
-        delay: 4750
-      }, defaultProps)
-  );
+  ScrollReveal().reveal('#contact>.container>.section-title', defaultProps);
+  ScrollReveal().reveal('.contact-wrapper', defaultProps);
 }
