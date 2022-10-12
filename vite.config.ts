89d718df5821f5c045859a3e30fc2069f565e69f
donno2048 <just4now666666@gmail.com>
2022-10-12 19:20:29 +0300

    auto format

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
 import { defineConfig } from 'vite';
 import htmlMinimize from '@sergeymakinen/vite-plugin-html-minimize';
 export default defineConfig({
-    root: './src',
-    build: {
-        outDir: '../dist',
-        assetsDir: '.',
-        emptyOutDir: true,
-        rollupOptions: {
-            output: {
-                entryFileNames: '[name].js',
-                chunkFileNames: '[name].js',
-                assetFileNames: '[name].[ext]'
-            }
-        },
-        sourcemap: true,
-    },
-    plugins: [htmlMinimize()],
-    base: './',
+	root: './src',
+	build: {
+		outDir: '../dist',
+		assetsDir: '.',
+		emptyOutDir: true,
+		rollupOptions: {
+			output: {
+				entryFileNames: '[name].js',
+				chunkFileNames: '[name].js',
+				assetFileNames: '[name].[ext]'
+			}
+		},
+		sourcemap: true,
+	},
+	plugins: [htmlMinimize()],
+	base: './',
 });
