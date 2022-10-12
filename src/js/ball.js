d890c990aff8aa869cec1bf8022c0921b90329f3
donno2048 <just4now666666@gmail.com>
2022-10-12 18:31:18 +0300

    refactor

--- src/js/ball.js
+++ src/js/ball.js
@@ -1,21 +1,21 @@
-function ballAnimation () {
-    const ball = document.getElementById('ball');
-    ball.addEventListener('mousedown', function (e) {
-        var y = e.clientY;
-        ball.style.backgroundColor = 'blue';
-        ball.style.color = 'transparent';
-        ball.style.userSelect = 'none';
-        ball.style.left = e.clientX + 'px';
-        ball.style.top = y + window.scrollY + 'px';
-        ball.style.cursor = 'none';
-        document.addEventListener('mousemove', function (e) {
-            y = e.clientY;
-            ball.style.left = e.clientX + 'px';
-            ball.style.top = y + window.scrollY + 'px';
-        });
-        window.addEventListener("scroll", function () {
-            ball.style.top = y + window.scrollY + 'px';
-        });
-    });
+function ballAnimation() {
+	const ball = document.getElementById('ball');
+	ball.addEventListener('mousedown', function (e) {
+		var y = e.clientY;
+		ball.style.backgroundColor = 'blue';
+		ball.style.color = 'transparent';
+		ball.style.userSelect = 'none';
+		ball.style.left = e.clientX + 'px';
+		ball.style.top = y + window.scrollY + 'px';
+		ball.style.cursor = 'none';
+		document.addEventListener('mousemove', function (e) {
+			y = e.clientY;
+			ball.style.left = e.clientX + 'px';
+			ball.style.top = y + window.scrollY + 'px';
+		});
+		window.addEventListener("scroll", function () {
+			ball.style.top = y + window.scrollY + 'px';
+		});
+	});
 }
 export default ballAnimation;
--- src/js/bubbleColor.js
+++ src/js/bubbleColor.js
@@ -1,9 +1,9 @@
 function Color() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+	var letters = '0123456789ABCDEF';
+	var color = '#';
+	for (var i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
 }
-window.addEventListener('load', function () {const bubbles = document.getElementsByClassName("bubbles")[0].childNodes;document.addEventListener('contextmenu', (e) => {e.preventDefault();bubbles[Math.floor(Math.random() * bubbles.length)].style.background=Color();}, false);})
+window.addEventListener('load', function () { const bubbles = document.getElementsByClassName("bubbles")[0].childNodes; document.addEventListener('contextmenu', (e) => { e.preventDefault(); bubbles[Math.floor(Math.random() * bubbles.length)].style.background = Color(); }, false); })
